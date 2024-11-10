d660528e018589a55f2bd04966cf054c0d30ab5d
Rafael Lima <rafaelglima@gmail.com>
2024-11-10 01:13:02 -0300

    Update app.py

--- lambdas/lambda-users-s3/app.py
+++ lambdas/lambda-users-s3/app.py
@@ -1,5 +1,9 @@
 import os
 import boto3
+import pymysql
+import datetime
+from datetime import datetime
+
 from chalice import Chalice
 
 app = Chalice(app_name='lambda-users-s3')
@@ -17,15 +21,54 @@ def s3_handler(event):
     try:
         #S3
         s3_client = boto3.client('s3')
-    
+
+        # banco de dados apenas para persistir os tempos para analise estatistica. alterar os valores.
+        host = ""
+        user = ""
+        password = ""
+        database = ""
+
+        timestamp1=datetime.now()
         # Download do arquivo do S3 vindo do script
         temp_file = "/tmp/" + event.key  #Caminho temporário para armazenar o arquivo
         s3_client.download_file(event.bucket, event.key, temp_file) #bucket, object_name, file_name
         
         # Upload do arquivo pro S3 da camada em nevoa
         s3_client.upload_file(temp_file, S3_BUCKET_DEST, event.key)
+        timestamp2=datetime.now()
+        
+        Tempo1=(timestamp2-timestamp1).total_seconds()
         
         print(f'O arquivo foi enviado para o S3')
+
+        try:
+                        
+            connection = pymysql.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=database
+            )
+            cursor = connection.cursor()
+
+                        
+            insert_query = """
+            INSERT INTO baseline (tempo1) 
+            VALUES (%s);
+            """
+            cursor.execute(insert_query, (Tempo0))
+                        
+                        
+            connection.commit()
+            print("Dados inseridos no MySQL com sucesso!")
+
+        except pymysql.MySQLError as db_error:
+            print(f"Erro ao inserir dados no MySQL: {db_error}")
+
+        finally:
+            if connection:
+                cursor.close()
+                connection.close()
 
     except Exception as e:
         print(e)
